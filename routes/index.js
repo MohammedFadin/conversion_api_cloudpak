7a1d84260376567d219e61770c94a95b6f27a208
mfadin <m.fadin@ibm.com>
2019-12-09 13:29:38 +0400

    new version 12

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { msg: "Hello Fadin to Cloud Pak!!" });
+  res.render("index", { msg: "Hello Fadin to Cloud Pak v3!!" });
 });
 
 router.get("/login", (req, res) => {
-  res.render("index", { msg: "Login page" });
+  res.render("index", { msg: "Login to your system" });
 });
 
 module.exports = router;
